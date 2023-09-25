41ca4d611a67d3b2a616d916fe6b2152a15b2bc6
Студент BStudentD <student@obe.local>
2023-09-25 18:05:59 +0300

    rotation&damagabale

---
+++ Assets/Scripts/DamagableComponent.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagableComponent : MonoBehaviour
+{
+    [SerializeField] int hp = 100;
+
+    int currentHp;
+
+    bool isDead;
+
+    public void Start()
+    {
+        currentHp = hp;
+    }
+
+    public bool IsDead => isDead;
+
+    public int Hp
+    {
+        get => currentHp;
+        set
+        {
+            if (isDead)
+                return;
+
+            currentHp = value;
+
+            if(currentHp <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log($"{gameObject.name} is dead");
+        isDead = true;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float forwardSpeed = 1;
     [SerializeField] float sideSpeed = 1;
+    [SerializeField] float sensitivity = 1;
 
     Rigidbody rb;
 
@@ -15,11 +16,20 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        float xMouseMovement = Input.GetAxis("Mouse X");
 
-        rb.AddForce(new Vector3(horizontal * Time.fixedDeltaTime * forwardSpeed, 0, vertical * Time.fixedDeltaTime * sideSpeed));
+        float rotation = xMouseMovement * sensitivity * Time.fixedDeltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        Vector3 force = Vector3.zero;
+        force += transform.forward * vertical * Time.fixedDeltaTime * forwardSpeed;
+        force += transform.right * horizontal * Time.fixedDeltaTime * sideSpeed;
+
+        rb.AddForce(force);
     }
 }
